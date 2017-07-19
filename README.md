878dca2694ee968fc6cb7c549c206fbf27c111b6
amirmocto / peterdemartini <amirm@citrix.com , thepeterdemartini@gmail.com>
2017-07-19 15:48:40 -0700

    v1.0.0 it is a library now!

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # storefront-ica-generator
-StoreFront ICA Generator
+
+[![Dependency status](http://img.shields.io/david/octoblu/storefront-ica-generator.svg?style=flat)](https://david-dm.org/octoblu/storefront-ica-generator)
+[![devDependency Status](http://img.shields.io/david/dev/octoblu/storefront-ica-generator.svg?style=flat)](https://david-dm.org/octoblu/storefront-ica-generator#info=devDependencies)
+[![Build Status](http://img.shields.io/travis/octoblu/storefront-ica-generator.svg?style=flat&branch=master)](https://travis-ci.org/octoblu/storefront-ica-generator)
+[![Slack Status](http://community-slack.octoblu.com/badge.svg)](http://community-slack.octoblu.com)
+
+[![NPM](https://nodei.co/npm/storefront-ica-generator.svg?style=flat)](https://npmjs.org/package/storefront-ica-generator)
+
+## License
+
+The MIT License (MIT)
+
+Copyright 2017 Octoblu Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ test/storefront-service-spec.js
@@ -0,0 +1,164 @@
+const { describe, beforeEach, afterEach } = global
+const { expect, it } = global
+const shmock = require("@octoblu/shmock")
+const enableDestroy = require("server-destroy")
+const url = require("url")
+const StoreFrontService = require('../')
+
+describe('StoreFrontService', function() {
+  beforeEach('setup', function () {
+    this.storeFrontServer = shmock()
+    enableDestroy(this.storeFrontServer)
+    this.storeFrontUrl = url.format({
+      hostname: 'localhost',
+      pathname: 'Citrix/StoreWeb',
+      port: this.storeFrontServer.address().port,
+      protocol: 'http',
+    })
+    this.sut = new StoreFrontService({
+      storeFrontUrl: this.storeFrontUrl,
+      domain: 'some-domain',
+      username: 'some-username',
+      password: 'some-password',
+      desktop: 'some-desktop-name',
+    })
+  })
+
+  afterEach(function () {
+    this.storeFrontServer.destroy()
+  })
+
+  describe('->generateICA', function() {
+    beforeEach('generate-ica', function(done) {
+      this.loadMainPage = this.storeFrontServer.get('/Citrix/StoreWeb')
+        .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+        .set('Upgrade-Insecure-Requests', '1')
+        .reply(200, {}, {
+          'Set-Cookie': 'CsrfToken=some-csrf-token'
+        })
+
+      this.homeConfiguration = this.storeFrontServer.post('/Citrix/StoreWeb/Home/Configuration')
+        .set('Accept', 'application/xml, text/xml, */*; q=0.01')
+        .set('X-Requested-With', 'XMLHttpRequest')
+        .set('X-Citrix-IsUsingHTTPS', 'Yes')
+        .set('Referer', this.storeFrontUrl)
+        .set('Content-Length', '0')
+        .reply(200, {}, {
+          'Set-Cookie': 'CsrfToken=some-csrf-token'
+        })
+
+      this.checkResources = this.storeFrontServer.post('/Citrix/StoreWeb/Resources/List')
+        .set('Content-Type', 'application/x-www-form-urlencoded')
+        .set('Accept', 'application/json, text/javascript, */*; q=0.01')
+        .set('X-Citrix-IsUsingHTTPS', "Yes")
+        .set('Csrf-Token', 'some-csrf-token')
+        .set('Referer', this.storeFrontUrl)
+        .send({
+          format: 'json',
+          resourceDetails: 'Full',
+        })
+        .reply(200)
+
+      this.getAuthMethods = this.storeFrontServer.post('/Citrix/StoreWeb/Authentication/GetAuthMethods')
+        .set('Accept', 'application/xml, text/xml, */*; q=0.01')
+        .set('X-Citrix-IsUsingHTTPS', "Yes")
+        .set('Csrf-Token', 'some-csrf-token')
+        .set('Referer', this.storeFrontUrl)
+        .set('Content-Length', '0')
+        .reply(200)
+
+      this.explicitLogin = this.storeFrontServer.post('/Citrix/StoreWeb/ExplicitAuth/Login')
+        .set('Accept', 'application/xml, text/xml, */*; q=0.01')
+        .set('X-Citrix-IsUsingHTTPS', "Yes")
+        .set('Csrf-Token', 'some-csrf-token')
+        .set('Content-Length', '0')
+        .reply(200)
+
+      this.explicitLoginAttempt = this.storeFrontServer.post('/Citrix/StoreWeb/ExplicitAuth/LoginAttempt')
+        .set('Accept', 'application/xml, text/xml, */*; q=0.01')
+        .set('Accept-Language', 'en-US,en;q=0.8')
+        .set('X-Requested-With', 'XMLHttpRequest')
+        .set('X-Citrix-IsUsingHTTPS', "Yes")
+        .set('Csrf-Token', 'some-csrf-token')
+        .set('Referer', this.storeFrontUrl)
+        .send({
+          domain: 'some-domain',
+          loginBtn: 'Log On',
+          password: 'some-password',
+          saveCredentials: "true",
+          username: 'some-username',
+          StateContext: '',
+        })
+        .reply(200)
+
+      this.listResources = this.storeFrontServer.post('/Citrix/StoreWeb/Resources/List')
+        .set('Content-Type', 'application/x-www-form-urlencoded')
+        .set('Accept', 'application/json, text/javascript, */*; q=0.01')
+        .set('X-Citrix-IsUsingHTTPS', "Yes")
+        .set('Csrf-Token', 'some-csrf-token')
+        .set('Referer', this.storeFrontUrl)
+        .send({
+          format: 'json',
+          resourceDetails: 'Full',
+        })
+        .reply(200, {
+          resources: [
+            { name: 'some-desktop-name', type: 'Citrix.MPS.Desktop', launchurl: '/some-launch-id.ica' },
+            { name: 'some-desktop-name', type: 'Citrix.MPS.Application', launchurl: 'wrong' },
+            { name: 'some-other-name', type: 'Citrix.MPS.Desktop', launchurl: 'wrong' },
+          ]
+        })
+
+      this.getICAFileContents = this.storeFrontServer.get('/Citrix/StoreWeb/some-launch-id.ica')
+        .query({
+          CsrfToken: 'some-csrf-token',
+          IsUsingHttps: 'Yes',
+        })
+        .reply(200, 'some-ica-file-content')
+
+      this.sut.generateICA((error, icaContents) => {
+        if (error) {
+          return done(error)
+        }
+        this.icaContents = icaContents
+        done()
+      })
+    })
+
+    it('should yeild ICA contents', function () {
+      expect(this.icaContents).to.deep.equal('some-ica-file-content')
+    })
+
+    it('should call load main page', function () {
+      this.loadMainPage.done()
+    })
+
+    it('should call home configuration', function () {
+      this.homeConfiguration.done()
+    })
+
+    it('should check list resources', function () {
+      this.checkResources.done()
+    })
+
+    it('should call get auth methods', function () {
+      this.getAuthMethods.done()
+    })
+
+    it('should call explicit login', function () {
+      this.explicitLogin.done()
+    })
+
+    it('should attempt to login', function () {
+      this.explicitLoginAttempt.done()
+    })
+
+    it('should really get the resources', function () {
+      this.listResources.done()
+    })
+
+    it('should get the ica file contents', function() {
+      this.getICAFileContents.done()
+    })
+  })
+})
